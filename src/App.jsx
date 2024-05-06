8c344aeb522551d42abc4b53494df6b11adb22c8
kahouechi <uechikaho@gmail.com>
2024-05-06 10:02:09 -0400

    creating basic app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import ScrollToTop from './utils/ScrollToTop';
+import Layout from './Layout';
+import Home from './pages/Home.jsx';
+
+const App = () => {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+function Layout() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Layout
