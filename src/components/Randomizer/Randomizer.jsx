ed7d1f4d15497661b12fc3f572194f67332ea747
kahouechi <uechikaho@gmail.com>
2024-05-07 10:38:22 -0400

    style randomize button

--- src/components/Randomizer/Randomizer.jsx
+++ src/components/Randomizer/Randomizer.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
 import options from '../../data/options.json'
+import './Randomizer.css'
 
 const Randomizer = () => {
 
   const [randomOption, setRandomOption] = useState(null);
   const randomize = () => {
     setRandomOption(Math.floor(Math.random() * options.length))
-    console.log(randomOption);
+    
   }
 
 
   return (
-    <div>
-      <button onClick={randomize}>Randomize</button>
-      <div>{randomOption !== null && options[randomOption].name}</div>
+    <div className='randomizer-wrapper'>
+      <button onClick={randomize}>RANDOMIZE!!!</button>
+      <p>{randomOption !== null && options[randomOption].name}</p>
+      
     </div>
   )
 }
