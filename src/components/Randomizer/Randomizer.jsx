21d37197b2d3e7cc1cd209cce402d00c2839b663
kahouechi <uechikaho@gmail.com>
2024-05-07 10:27:31 -0400

    aye randomizer

--- src/components/Randomizer/Randomizer.jsx
+++ src/components/Randomizer/Randomizer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import options from '../../data/options.json'
 
 const Randomizer = () => {
+
+  const [randomOption, setRandomOption] = useState(null);
+  const randomize = () => {
+    setRandomOption(Math.floor(Math.random() * options.length))
+    console.log(randomOption);
+  }
+
+
   return (
-    <button>Randomizer</button>
+    <div>
+      <button onClick={randomize}>Randomize</button>
+      <div>{randomOption !== null && options[randomOption].name}</div>
+    </div>
   )
 }
 
