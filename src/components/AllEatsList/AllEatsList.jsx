8439d2b57ca7bce973432aa95276b85b47ce5a23
kahouechi <uechikaho@gmail.com>
2024-05-08 11:29:11 -0400

    starting on filtering feauture

--- src/components/AllEatsList/AllEatsList.jsx
+++ src/components/AllEatsList/AllEatsList.jsx
@@ -3,17 +3,35 @@ import options from '../../data/options.json'
 import './AllEatsList.css'
 
 const AllEats = () => {
+
+
+
   return (
-    <div className='all-eats-container'>
-      {options.map((option, i) => (
-        <div key={option.id} className='option'>
-          <video width="50" height="50" playsinline autoplay muted loop controls className='overflow-hidden'>
-            <source src="./GriddyBattle.mp4" type="video/mp4" />
-            Your browser does not support the video tag :&#40;
-          </video>
-          <h1>{option.name}</h1>
-        </div>
-      ))}
+    <div className="eats-wrapper">
+      <div className="sort-buttons">
+        <label for="cars">Filter:</label>
+        <select name='filter'>
+          <option>None</option>
+          <option>Sweet</option>
+          <option>Savoury</option>
+          <option>Drinks</option>
+          <option>Food</option>
+          <option>Meal</option>
+          <option>Snack</option>
+        </select>
+      </div>
+
+      <div className='options-container'>
+        {options.map((option, i) => (
+          <div key={option.id} className='option'>
+            <video width="50" height="50" playsinline autoplay muted loop controls className='overflow-hidden'>
+              <source src="./GriddyBattle.mp4" type="video/mp4" />
+              Your browser does not support the video tag :&#40;
+            </video>
+            <h1>{option.name}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
